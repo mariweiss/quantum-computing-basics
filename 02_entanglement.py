9a70796a8bc45f8e97057f6ffe4c5ce38b9eb67e
Mariana <91998518+mariweiss@users.noreply.github.com>
2026-05-16 13:28:23 -0300

    Create 02_entanglement.py

---
+++ 02_entanglement.py
@@ -0,0 +1,18 @@
+from qiskit import QuantumCircuit, Aer, execute
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+qc = QuantumCircuit(2, 2)
+
+qc.h(0)
+qc.cx(0, 1)
+
+qc.measure([0,1], [0,1])
+
+simulator = Aer.get_backend('qasm_simulator')
+result = execute(qc, simulator, shots=1000).result()
+counts = result.get_counts()
+
+print(counts)
+plot_histogram(counts)
+plt.show()
